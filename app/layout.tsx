571cbcd9e1211e754c558bae54eb80e5ff380b1b
Harun11100 <145543162+Harun11100@users.noreply.github.com>
2024-10-20 22:19:09 +0600

    All the assets are created

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 
 import "./globals.css";
 import { Inter,Space_Grotesk} from 'next/font/google'
+import Navbar from "@/components/Navbar";
 
 const inter =Inter({subsets:['latin']})
 const spaceGrotesk =Space_Grotesk({subsets:['latin'],weight:['300','400','500','600','700']})
@@ -19,7 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {children}
+        <main className="max-w-10xl mx-auto">
+            <Navbar/>
+            {children}
+        </main>
+       
       </body>
     </html>
   );
